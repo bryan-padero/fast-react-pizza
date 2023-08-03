78deb8eed64cbd7b6f27b9f47c16352a6c42f7f0
Bryan <bryanpadero@gmail.com>
2023-08-03 11:14:29 +0400

    added user reducer

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import LinkButton from "../../ui/LinkButton";
 import CartItem from "./CartItem";
@@ -29,6 +30,8 @@ const fakeCart = [
 function Cart() {
   const cart = fakeCart;
 
+  const username = useSelector((state) => state.user.username);
+
   return (
     <div className="px-4 py-3">
       <LinkButton
@@ -38,7 +41,7 @@ function Cart() {
         &larr; Back to menu
       </LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
